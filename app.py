e2015d8e741cc9b1c7629a0b7d0aefa07427c675
markmarkmadara <72902563+markmarkmadara@users.noreply.github.com>
2023-06-20 18:38:39 +0800

    Working Dropdown list with Percent Changes Indication Number Years

--- app.py
+++ app.py
@@ -112,11 +112,38 @@ def forecasting():
                 }
             )
 
+            # Step 9: Get percentage change
+            percent_changes = []
+            start_index = 0
+            for i in range(4):
+                end_index = -(4 - i)
+                pd_change = (
+                    prediction_df["Forecast"].iloc[end_index]
+                    - prediction_df["Forecast"].iloc[start_index]
+                ) / prediction_df["Forecast"].iloc[start_index]
+                percent_changes.append(round(pd_change * 100, 2))
+                start_index += 1
+
+            # Calculate average percentage change
+            average_percent_change = sum(percent_changes) / len(percent_changes)
+
+            # Format percent changes for display
+            formatted_changes = []
+            for i in range(4):
+                quarter = i + 1
+                year_start = prediction_df["Year"].iloc[start_index - 1]
+                year_end = prediction_df["Year"].iloc[end_index]
+                formatted_changes.append(
+                    f"Q{quarter} {year_start}-{year_end}: {percent_changes[i]:.2f}%"
+                )
+
             # Render the forecasting_results.html template with the predicted data
             return render_template(
                 "forecasting_results.html",
                 prediction_df=prediction_df.to_dict(orient="records"),
                 number=number,
+                formatted_changes=formatted_changes,
+                average_percent_change=average_percent_change,
             )
 
         elif selected_option == "VolumeProduction":
@@ -172,14 +199,46 @@ def forecasting():
                 }
             )
 
+            # Step 9: Get percentage change
+            percent_changes = []
+            start_index = 0
+            for i in range(4):
+                end_index = -(4 - i)
+                pd_change = (
+                    prediction_df["Forecast"].iloc[end_index]
+                    - prediction_df["Forecast"].iloc[start_index]
+                ) / prediction_df["Forecast"].iloc[start_index]
+                percent_changes.append(round(pd_change * 100, 2))
+                start_index += 1
+
+            # Calculate average percentage change
+            average_percent_change = sum(percent_changes) / len(percent_changes)
+
+            # Format percent changes for display
+            formatted_changes = []
+            for i in range(4):
+                quarter = i + 1
+                year_start = prediction_df["Year"].iloc[start_index - 1]
+                year_end = prediction_df["Year"].iloc[end_index]
+                formatted_changes.append(
+                    f"Q{quarter} {year_start}-{year_end}: {percent_changes[i]:.2f}%"
+                )
+
             # Render the forecasting_results.html template with the predicted data
             return render_template(
                 "forecasting_volumeResult.html",
                 prediction_df=prediction_df.to_dict(orient="records"),
+                number=number,
+                formatted_changes=formatted_changes,
+                average_percent_change=average_percent_change,
             )
+
         elif selected_option == "FarmgatePrices":
             # Step 1: Load the data
             data = pd.read_csv("FarmgatePrices.csv")
+
+            # To avoid using scientific notation
+            pd.set_option("display.float_format", lambda x: "%d" % x)
 
             # Convert 'Year' column to string type
             data["Year"] = data["Year"].astype(str)
@@ -230,12 +289,130 @@ def forecasting():
                 }
             )
 
+            # Step 9: Get percentage change
+            percent_changes = []
+            start_index = 0
+            for i in range(4):
+                end_index = -(4 - i)
+                pd_change = (
+                    prediction_df["Forecast"].iloc[end_index]
+                    - prediction_df["Forecast"].iloc[start_index]
+                ) / prediction_df["Forecast"].iloc[start_index]
+                percent_changes.append(round(pd_change * 100, 2))
+                start_index += 1
+
+            # Calculate average percentage change
+            average_percent_change = sum(percent_changes) / len(percent_changes)
+
+            # Format percent changes for display
+            formatted_changes = []
+            for i in range(4):
+                quarter = i + 1
+                year_start = prediction_df["Year"].iloc[start_index - 1]
+                year_end = prediction_df["Year"].iloc[end_index]
+                formatted_changes.append(
+                    f"Q{quarter} {year_start}-{year_end}: {percent_changes[i]:.2f}%"
+                )
+
             # Render the forecasting_results.html template with the predicted data
             return render_template(
                 "forecasting_pricesResult.html",
                 prediction_df=prediction_df.to_dict(orient="records"),
                 number=number,
+                formatted_changes=formatted_changes,
+                average_percent_change=average_percent_change,
             )
+
+        elif selected_option == "VolumeDemand":
+            # Step 1: Load the data
+            data = pd.read_csv("VolumeDemand.csv")
+
+            # To avoid using scientific notation
+            pd.set_option("display.float_format", lambda x: "%d" % x)
+
+            # Convert 'Year' column to string type
+            data["Year"] = data["Year"].astype(str)
+            data["YearQuarter"] = data["Year"] + "-" + data["TimePeriod"]
+
+            data["VolumeDemand"] = np.log(data["VolumeDemand"])
+            train_data = data["VolumeDemand"].iloc[: int(len(data) * 0.7)]
+            train_data_diff = train_data.diff().dropna()
+
+            # Step 3: Fit the ARIMA model
+            model = ARIMA(train_data, order=(4, 1, 0))
+            model_fit = model.fit()
+
+            # Step 4: Make time series predictions
+            test_data = data["VolumeDemand"].iloc[int(len(data) * 0.7) :]
+            forecast = model_fit.forecast(steps=len(test_data))
+
+            # Step 2: Fit the ARIMA model
+            model = ARIMA(data["VolumeDemand"], order=(4, 1, 0))
+            model_fit = model.fit()
+
+            combined_data = pd.concat(
+                [data[["YearQuarter", "VolumeDemand"]], pd.Series(forecast)],
+                axis=1,
+            )
+            combined_data.columns = ["Year", "Actual", "Forecast"]
+
+            # Fit the ARIMA model using the actual series
+            train_data = data["VolumeDemand"].iloc[: int(len(data) * 0.7)]
+            model = ARIMA(train_data, order=(4, 1, 0))
+            model_fit = model.fit()
+
+            # Step 3: Make time series predictions
+            last_year = int(data["Year"].iloc[-1])
+            last_year = last_year + 1
+            future_years = pd.date_range(
+                start=f"{last_year}-01-01", periods=num_years * 4, freq="Q"
+            )
+            forecast = pd.Series(model_fit.forecast(steps=num_years * 4).values)
+
+            # Step 4: Create a DataFrame with the predicted data
+            prediction_df = pd.DataFrame(
+                {
+                    "Year": future_years.year,
+                    "TimePeriod": future_years.quarter,
+                    "Actual": data["VolumeDemand"].values[-num_years * 4 :],
+                    "Forecast": forecast,
+                }
+            )
+
+            # Step 9: Get percentage change
+            percent_changes = []
+            start_index = 0
+            for i in range(4):
+                end_index = -(4 - i)
+                pd_change = (
+                    prediction_df["Forecast"].iloc[end_index]
+                    - prediction_df["Forecast"].iloc[start_index]
+                ) / prediction_df["Forecast"].iloc[start_index]
+                percent_changes.append(round(pd_change * 100, 2))
+                start_index += 1
+
+            # Calculate average percentage change
+            average_percent_change = sum(percent_changes) / len(percent_changes)
+
+            # Format percent changes for display
+            formatted_changes = []
+            for i in range(4):
+                quarter = i + 1
+                year_start = prediction_df["Year"].iloc[start_index - 1]
+                year_end = prediction_df["Year"].iloc[end_index]
+                formatted_changes.append(
+                    f"Q{quarter} {year_start}-{year_end}: {percent_changes[i]:.2f}%"
+                )
+
+            # Render the forecasting_results.html template with the predicted data
+            return render_template(
+                "forecasting_volumeDemandResult.html",
+                prediction_df=prediction_df.to_dict(orient="records"),
+                number=number,
+                formatted_changes=formatted_changes,
+                average_percent_change=average_percent_change,
+            )
+
     # Render the forecasting.html template for user input
     return render_template("forecasting.html")
 
